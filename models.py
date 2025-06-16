c3e5cf96a12905aa40108e3d68ea38b6c013f781
zhaaamal <madiarovanurjamal@gmail.com>
2025-06-16 20:28:39 +0600

    add models as Photo and FaceEncoding

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+from ctypes.wintypes import OLESTR
+
+from pydantic.color import Color
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship, declarative_base
 
@@ -13,4 +16,33 @@ class Person(Base):
 
     face_encodings = relationship("FaceEncoding", back_populates="person")
     photos = relationship("Photo", back_populates="person")
+
+
+class FaceEncoding(Base):
+    __tablename__ = 'face_encodings'
+
+    id = Column(Integer, primary_key=True)
+    encoding = Column(Text, nullable=False) # Сохраняем JSON/строку
+    person_id = Column(Integer, ForeignKey('persons.id'))
+
+    person = relationship("Person", back_populates="face_encodings")
+
+
+class Photo(Base):
+    __tablename__ = 'photos'
+
+    id = Column(Integer, primary_key=True)
+    filepath = Column(String, nullable=False)
+    person_id = Column(Integer, ForeignKey('persons.id'))
+
+    person = relationship("Person", back_populates="photos")
+
+
+
+
+
+
+
+
+
 
